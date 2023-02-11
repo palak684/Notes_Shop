e1a01abe97f1407285d21c0058bdbafa24a1598e
Pratiksha Goswami <104529563+Pratikshathisside@users.noreply.github.com>
2023-02-11 22:29:33 +0530

    Add files via upload

---
+++ services/firebase_services.dart
@@ -0,0 +1,34 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseServices {
+  final _auth = FirebaseAuth.instance;
+  final _googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? _user;
+  GoogleSignInAccount get user => _user!;
+
+  signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await _googleSignIn.signIn();
+
+      if (googleSignInAccount != null) {
+        final GoogleSignInAuthentication googleSignInAuthentication =
+            await googleSignInAccount.authentication;
+        final AuthCredential authCredential = GoogleAuthProvider.credential(
+            accessToken: googleSignInAuthentication.accessToken,
+            idToken: googleSignInAuthentication.idToken);
+        await _auth.signInWithCredential(authCredential);
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e.message);
+      throw e;
+    }
+  }
+
+  signOut() async {
+    await _auth.signOut();
+    await _googleSignIn.signOut();
+  }
+}
