f66ef9ba4aaf18213632e6acae354a0e242c49e0
Pratiksha Goswami <104529563+Pratikshathisside@users.noreply.github.com>
2023-02-16 18:56:44 +0530

    Update firebase_services.dart

--- services/firebase_services.dart
+++ services/firebase_services.dart
@@ -9,26 +9,26 @@ class FirebaseServices {
   GoogleSignInAccount get user => _user!;
 
   signInWithGoogle() async {
-    try {
-      final GoogleSignInAccount? googleSignInAccount =
-          await _googleSignIn.signIn();
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser =
+        await GoogleSignIn(scopes: <String>["email"]).signIn();
 
-      if (googleSignInAccount != null) {
-        final GoogleSignInAuthentication googleSignInAuthentication =
-            await googleSignInAccount.authentication;
-        final AuthCredential authCredential = GoogleAuthProvider.credential(
-            accessToken: googleSignInAuthentication.accessToken,
-            idToken: googleSignInAuthentication.idToken);
-        await _auth.signInWithCredential(authCredential);
-      }
-    } on FirebaseAuthException catch (e) {
-      print(e.message);
-      throw e;
-    }
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
-  signOut() async {
-    await _auth.signOut();
-    await _googleSignIn.signOut();
+  //Sign out
+  signOut() {
+    FirebaseAuth.instance.signOut();
   }
 }
