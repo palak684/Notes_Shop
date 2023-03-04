5271e2c2f2d1be82e4a58334e9eaa7c7174a273e
prathna17 <101191188+prathna17@users.noreply.github.com>
2023-03-04 17:41:55 +0530

    Update login_teacher.dart

--- login_teacher.dart
+++ login_teacher.dart
@@ -1,6 +1,8 @@
-import 'package:firebase_project/screens/home_teacher.dart';
+import 'package:firebase_project/screens/reusable_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
+import 'home_teacher.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -16,104 +18,59 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.purple[800],
+          leading:
+          IconButton( onPressed: (){
+            Navigator.pop(context);
+          },icon:Icon(Icons.arrow_back_ios,size: 20,color: Colors.black,)),
+        ),
         body: Container(
           decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/Register.png'), fit: BoxFit.cover
-            ),
+            image: DecorationImage(image: AssetImage('assets/Register (1).png'), fit: BoxFit.cover),
           ),
-          child: ListView(
-            children: <Widget>[
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [Column(children:[
+
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                child: TextField (
-                  scrollPadding: EdgeInsets.only(
-                      bottom: MediaQuery.of(context).viewInsets.bottom),
-                  controller: _EmailTextController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      labelText: 'Enter Email ID',
-                      filled: true,
-                      fillColor: Colors.purple[200]
+                padding: EdgeInsets.symmetric(
+                    horizontal: 40
+                ),
+                child:Column(children:[
+                  reusableTextField("Enter Email", Icons.email, false, _EmailTextController),
+                  SizedBox(
+                    height: 30,
                   ),
+                ],
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                child: TextField (
-                  scrollPadding: EdgeInsets.only(
-                      bottom: MediaQuery.of(context).viewInsets.bottom),
-                  controller: _passwordTextController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      labelText: 'Enter Password',
-                      filled: true,
-                      fillColor: Colors.purple[200]
+                padding: EdgeInsets.symmetric(
+                    horizontal: 40
+                ),
+
+                child:Column(children: [
+                  reusableTextField("Enter Password", Icons.lock_outline, true, _passwordTextController),
+                  SizedBox(
+                    height: 20,
                   ),
+                ],
                 ),
               ),
-              new SizedBox(
-                  width: 220,
-                  height: 40,
-                  child: FloatingActionButton.extended(
-                      heroTag: 'login',
-                      onPressed: (){
-                        FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
-                            email: _EmailTextController.text,
-                            password: _passwordTextController.text)
-                            .then((value) {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => CSE_T()));
-                        }).onError((error, stackTrace) {
-                          print("Error ${error.toString()}");
-                        });
-                      },
-                      label: Text('Login',style: TextStyle(color: Colors.black)),
-                      backgroundColor: Colors.white
-                  )
-              ),
-              Text(
-                  'OR',
-                  style: TextStyle(fontSize: 35,color: Colors.black)
-              ),
-              new SizedBox(
-                  width: 220,
-                  height: 40,
-                  child: FloatingActionButton.extended(
-                    heroTag: 'google',
-                    onPressed: (){},
-                    label: Text('Login in with Google'),
-                    icon: Image.asset(
-                        'assets/google icon.jpg',
-                        height: 30,
-                        width: 30),
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                  )
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              new SizedBox(
-                width: 220,
-                height: 40,
-                child: FloatingActionButton.extended(
-                  heroTag: 'apple',
-                  onPressed: (){},
-                  label: Text('Login in with Apple'),
-                  icon: Image.asset(
-                      'assets/apple.png',
-                      height: 32,
-                      width: 32),
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.black,
-                ),
-              ),
+              SigninSignupButton(context, true, (){
+                FirebaseAuth.instance.signInWithEmailAndPassword(email: _EmailTextController.text,
+                    password: _passwordTextController.text).then((value)
+                {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CSE_T()),
+                  );
+                });
+              },)
+            ],
+            ),
             ],
           ),
         ),
